72a8841ee6c004c16d84b131adfcd914f60da143
Flexible1337 <67372386+Flexible1337@users.noreply.github.com>
2020-08-16 23:52:05 +0800

    Create load.lua

---
+++ load.lua
@@ -0,0 +1,2 @@
+client.color_log(118, 160, 234, 'Ty for using emiya private lua;感谢VEL后台的支持和帮助；杜绝盗用倒卖泄露无脑夸大!')
+client.color_log(255, 100, 100, 'Ty for using emiya private lua;感谢VEL后台的支持和帮助；杜绝盗用倒卖泄露无脑夸大!')
