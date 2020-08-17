4510eb86bc3b86dcb510ba3fd4d95ff529f2d2c6
Flexible1337 <67372386+Flexible1337@users.noreply.github.com>
2020-08-17 19:23:17 +0800

    Create load_callback.lua

---
+++ load_callback.lua
@@ -0,0 +1,2 @@
+client.color_log(131, 176, 255, "感谢使用EMIYA私人LUA以及VEL对本人的大力支持，TY FOR USING EMIYA PRIVATE LUA PROJECTS;杜绝泄露盗用倒卖等垃圾行为;选择VEL!")
+client.color_log(255, 129, 129, "感谢使用EMIYA私人LUA以及VEL对本人的大力支持，TY FOR USING EMIYA PRIVATE LUA PROJECTS;杜绝泄露盗用倒卖等垃圾行为;选择VEL!")
